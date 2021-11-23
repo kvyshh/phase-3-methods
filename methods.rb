6a217568f65a02a229755d95305bf191fc770214
Kindred Lee <kindred4lee@gmail.com>
2021-11-23 04:32:02 -0800

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default "Sunny"
+
+def add num1, num2
+    num1+num2
+end
+add 1,2
+
+def halve num=4
+    return nil unless num.class == Integer
+    num/2
+end
+
+halve 10
